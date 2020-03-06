33cfeb28b7bdd46da1953ae5b30575dad5fd0c07
Joey Petrone <joey.petrone@gmail.com>
2020-03-05 21:42:29 -0600

    Eat Quadrant complete

---
+++ src/javascripts/components/eatQuad.js
@@ -0,0 +1,15 @@
+import eatData from '../helpers/data/eatData';
+import utils from '../helpers/utils';
+
+const eatQuadBuilder = () => {
+  const fullness = eatData.getFullScore();
+  let domString = '';
+
+  domString += '<h5>Eat</h5>';
+  domString += `<h6>Full Score: ${fullness}</h6>`;
+  domString += '<button id="healthy-food-btn">Healthy Food</button><button id="unhealthy-food-btn">Unhealthy Food</button>';
+
+  utils.printToDom('eat', domString);
+};
+
+export default { eatQuadBuilder };
---
+++ src/javascripts/helpers/data/eatData.js
@@ -0,0 +1,16 @@
+
+let full = 70;
+
+const getFullScore = () => full;
+
+const setFullScore = (eatNum) => {
+  if (eatNum + full > 100) {
+    full = 100;
+  } else if (eatNum + full < 0) {
+    full = 0;
+  } else {
+    full += eatNum;
+  }
+};
+
+export default { getFullScore, setFullScore };
